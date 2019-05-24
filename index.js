acd9af419ace022b16ff96dea63835dc3864134f
SeyedMortezaKamali <smkplus3d@gmail.com>
2019-05-24 23:06:09 +0430

    Fix

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/chat.html', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+})
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
