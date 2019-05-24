5df5ebd1c797478082c8bddae6965e88ba671131
SeyedMortezaKamali <smkplus3d@gmail.com>
2019-05-24 20:56:39 +0430

    awdwa

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.use(express.static('/index.html'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
