7a67dc2058ae02be72e1f59ccb6fd795bbae8ec3
SeyedMortezaKamali <smkplus3d@gmail.com>
2019-05-29 13:54:14 +0430

    Fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongo = require('mongodb').MongoClient;
 const dbName = 'chat';
 
 // mongodb+srv://smkplus3d:!qazxsw2@cluster0-rporp.mongodb.net/test?retryWrites=true
-mongo.connect('mongodb+srv://smkplus3d:!qazxsw2@cluster0-rporp.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true }, function(err, client){
+mongo.connect('MONGODB',{ useNewUrlParser: true }, function(err, client){
   if(err){
     throw err;
   }
